49109f9f67444b8739809aade820994e86016f71
Esther Nehemiah <105179203+CheeryCodes@users.noreply.github.com>
2024-04-24 03:27:37 -0700

    Create my_levenshtein.js

---
+++ my_levenshtein.js
@@ -0,0 +1,12 @@
+function my_levenshtein(string1, string2){
+if (string1.length !== string2.length){
+    return -1;
+}
+let difference = 0;
+for(i=0; i<string1.length;i++){
+    if (string1[i] !== string2[i]){
+        difference++;
+    }
+}
+return difference;
+}
